e99f3adbe5a2faa068cfbb93e41d8177b1c07cef
adson.silva <adson.silva@ccc.ufcg.edu.br>
2017-02-14 11:48:46 -0300

    adição do .jar

---
+++ target/classes/static/js/index.js
@@ -0,0 +1,20 @@
+var trashIco = '<span class="glyphicon glyphicon-trash deleteTask"></span>';
+
+var checkBoxes, boxesChekeds;
+
+function changeBG() {
+    $('body').css("background-color","#dccfd8");
+}
+
+function increaseFont() {
+    $('body').css({"font-size":"20px"});
+}
+
+function decreaseFont() {
+    $('body').css({"font-size":"14px"});
+}
+
+function reset() {
+    $('body').css("background-color","#ffffff");
+    $('body').css({"font-size":"14px"});
+}
